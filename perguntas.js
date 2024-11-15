692c346c02355282db04d6af552b169d7be4efd7
witor2902 <00001131939724sp@al.educacao.sp.gov.br>
2024-11-14 23:45:56 -0300

    Add files via upload
    Projeto concluido

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Título nacional',
+    'Quado foi o primeiro título nacional conquistado pelo Flamengo?',
+    'Campeonato Brasileiro em 1980'
+)
+
+criaCartao(
+    'Campeonato Brasileiro',
+    'Quantas vezes o Flamengo já conquistou o Campeonato Brasileiro?',
+    'Oito vezes(1980, 1982, 1983, 1992, 2009, 2019, 2020, 2021).'
+)
+
+criaCartao(
+    'Goleada',
+    'Qual foi a maior goleada já aplicada pelo Flamenfo em um jogo oficial ?',
+    'Flamengo 15x1 contra o Mangueira, em 1920'
+)
+
+criaCartao(
+    'Maracanã',
+    'Qual o recore de público em um jogo do Flamengo no Maracanã?',
+    'Cerca de 194 mil pessoas, em 1963, contra o Fluminese'
+)
